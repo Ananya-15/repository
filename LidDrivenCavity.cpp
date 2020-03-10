55b2dca24f31e7593881cbc244ba2c0ac0d3d6cd
Ananya-15 <ananya.dubey17@imperial.ac.uk>
2020-03-10 20:20:34 +0000

    Altering command line inputs

--- LidDrivenCavity.cpp
+++ LidDrivenCavity.cpp
@@ -1,36 +1,65 @@
 
 #include "LidDrivenCavity.h"
+#include <iostream>
 
-LidDrivenCavity::LidDrivenCavity()
+LidDrivenCavity::LidDrivenCavity() //Constructor 
 {
+    
 }
 
-LidDrivenCavity::~LidDrivenCavity()
+LidDrivenCavity::~LidDrivenCavity() //Destructor 
 {
 }
 
 void LidDrivenCavity::SetDomainSize(double xlen, double ylen)
 {
+   Lx=xlen; 
+   Ly=ylen; 
 }
 
 void LidDrivenCavity::SetGridSize(int nx, int ny)
 {
+    Nx=nx; 
+    Ny=ny; 
 }
 
 void LidDrivenCavity::SetTimeStep(double deltat)
 {
+    dt=deltat; 
 }
 
 void LidDrivenCavity::SetFinalTime(double finalt)
 {
+    T=finalt; 
 }
 
 void LidDrivenCavity::SetReynoldsNumber(double re)
 {
+    Re=re;
 }
 
-void LidDrivenCavity::Initialise()
-{
+void LidDrivenCavity::Initialise(string *val) //Initialise all parameters
+{    
+    
+       double Lx=stod(val[0]); 
+       cout << Lx << endl; 
+       double Ly=stod(val[1]); 
+       cout << Ly << endl; 
+       int Nx=stoi(val[2]); 
+       cout << Nx << endl; 
+       int Ny=stoi(val[3]); 
+       cout << Ny << endl; 
+       int Px=stoi(val[4]); 
+       cout << Px << endl; 
+       int Py=stoi(val[5]);
+      cout << Py << endl;  
+       double dt=stod(val[6]);
+       cout << dt << endl; 
+       double T=stoi(val[7]); 
+       cout << T << endl; 
+       double Re=stod(val[8]); 
+       cout << Re << endl; 
+
 }
 
 void LidDrivenCavity::Integrate()
--- LidDrivenCavitySolver.cpp
+++ LidDrivenCavitySolver.cpp
@@ -3,31 +3,32 @@ using namespace std;
 
 #include "LidDrivenCavity.h"
 
+void printmat(int N, int M, string* A){ //matrix printing algorithm 
+for (int i = 0; i < N; i++){ //Array printing algo 
+           for (int j = 0; j < M; j++){
+            cout << A[i*M+j] << " ";
+            }
+          cout << endl;
+         }
+}
+
 int main(int argc, char **argv)
 {
     // Create a new instance of the LidDrivenCavity class
     LidDrivenCavity* solver = new LidDrivenCavity();
-    string inputs[argc-1]={"Lx","Ly","Nx","Ny","Px","Py","dt","T","Re"}; 
     
-     int n=argc; //Record no. of inputs 
-     double Ly=stod(argv[2]); 
-     int Nx=stoi(argv[4]); 
-     int Ny=stoi(argv[6]); 
-     int Px=stoi(argv[8]); 
-     int Py=stoi(argv[10]); 
-     double dt=stod(argv[12]);
-     double T=stoi(argv[14]); 
-     double Re=stod(argv[16]); 
-     
-     for(int i=0;i<argc/2;i++){
-         cout << inputs[i] << ": " << argv[2*(i+1)] << endl;
-     }
-     
-     
-//    double T; 
-//    double Re;  
-//    for(int i=1; i<argc; i++){
+    string inputs[(argc-1)/2]={"Lx","Ly","Nx","Ny","Px","Py","dt","T","Re"};
 
+    
+    int n=argc/2; //Record no. of inputs 
+    string *val=new string [n]; //Create array values, storing all the input values, to be passed as inputs to class function 
+    for (int i=0;i<n;i++){
+        val[i]=argv[2*(i+1)]; 
+    }
+     
+     solver->Initialise(val); 
+     
+     
 
 
     // Configure the solver here...
